529effc9a4e04fa2544ea4bfd22798d6978f3706
onkar-projects <kure.onkar41@gmail.com>
2024-04-12 13:03:47 +0530

    dot class added

--- src/pkg1/Hellow.java
+++ src/pkg1/Hellow.java
@@ -5,7 +5,5 @@ public class Hellow {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world");
-		System.out.println("new line 1");
-		System.out.println("new line 2");
 	}
 }
